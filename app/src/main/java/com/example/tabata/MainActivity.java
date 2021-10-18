4fcbca3ed77a6d7e52c81d5f6668e77a16dfc695
13floyd13 <patricemartin38@gmail.com>
2021-10-18 21:40:22 +0200

    first commit

---
+++ app/src/main/java/com/example/tabata/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tabata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onChoixEntrainement(View view) {
+    }
+}
