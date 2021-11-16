27d5753b400c541c5b19d529ce33b642deefb068
13floyd13 <patricemartin38@gmail.com>
2021-11-16 23:34:07 +0100

    changement des strings des boutons

--- app/src/main/java/controller/MenuCreation.java
+++ app/src/main/java/controller/MenuCreation.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tabata.R;
 
@@ -14,6 +16,31 @@ public class MenuCreation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_creation);
+
+        //récupération des Views
+        Button bCreationEntrainement = findViewById(R.id.btnCreationEntrainement);
+        Button bCreationSequence = findViewById(R.id.btnCreationSequence);
+        Button bCreationCycle = findViewById(R.id.btnCreationCycle);
+        Button bCreationTravail = findViewById(R.id.btnCreationTravail);
+
+        //Liste des strings à concatener pour set le text des boutons
+        String create = getResources().getString(R.string.creer);
+        String entrainement = getResources().getString(R.string.entrainement);
+        String sequence = getResources().getString(R.string.sequence);
+        String cycle = getResources().getString(R.string.cycle);
+        String travail = getResources().getString(R.string.travail);
+        String space = " ";
+        String createEntrainement = create + space + entrainement;
+        String createSequence = create + space + sequence;
+        String createCycle = create + space + cycle;
+        String createTravail = create + space + travail;
+
+        //Maj des boutons
+        bCreationEntrainement.setText(createEntrainement);
+        bCreationSequence.setText(createSequence);
+        bCreationCycle.setText(createCycle);
+        bCreationTravail.setText(createTravail);
+
     }
 
     public void onCreationEntrainement(View view) {
--- app/src/main/java/controller/MenuSuppression.java
+++ app/src/main/java/controller/MenuSuppression.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.tabata.R;
 
@@ -16,6 +17,30 @@ public class MenuSuppression extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_supression);
+
+        //récupération des Views
+        Button bSuppressionEntrainement = findViewById(R.id.btnSuppressionEntrainement);
+        Button bSuppressionSequence = findViewById(R.id.btnSuppressionSequence);
+        Button bSuppressionCycle = findViewById(R.id.btnSuppressionCycle);
+        Button bSuppressionTravail = findViewById(R.id.btnSuppressionTravail);
+
+        //Liste des strings à concatener pour set le text des boutons
+        String delete = getResources().getString(R.string.delete);
+        String entrainement = getResources().getString(R.string.entrainement);
+        String sequence = getResources().getString(R.string.sequence);
+        String cycle = getResources().getString(R.string.cycle);
+        String travail = getResources().getString(R.string.travail);
+        String space = " ";
+        String deleteEntrainement = delete + space + entrainement;
+        String deleteSequence = delete + space + sequence;
+        String deleteCycle = delete + space + cycle;
+        String deleteTravail = delete + space + travail;
+
+        //Maj des boutons
+        bSuppressionEntrainement.setText(deleteEntrainement);
+        bSuppressionSequence.setText(deleteSequence);
+        bSuppressionCycle.setText(deleteCycle);
+        bSuppressionTravail.setText(deleteTravail);
     }
 
     public void onSuppressionEntrainement(View view) {
