c7e6573cf21dcd3816a57ad490b080d5f4b4c791
13floyd13 <patricemartin38@gmail.com>
2021-11-24 19:24:06 +0100

    insertion d'un entrainement par défaut

--- app/src/main/java/data/DatabaseClient.java
+++ app/src/main/java/data/DatabaseClient.java
@@ -49,10 +49,49 @@ public class DatabaseClient {
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
             super.onCreate(db);
 
-            /*
-            db.execSQL("INSERT INTO task (task, description) VALUES(\"tâche 1\", \"description\");");
-            db.execSQL("INSERT INTO task (task, description) VALUES(\"tâche 2\", \"description\");");
-            */
+
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Pompes\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Abdos\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Squat\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Fentes\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Burpees\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Sauts\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Sprint\", 20, 10);");
+            db.execSQL("INSERT INTO travail (nom, temps, repos) VALUES(\"Montée de Genoux\", 20, 10);");
+
+            db.execSQL("INSERT INTO cycle (nom, repetition) VALUES(\"cycle pompes + squat\", 2);");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (1, 1)");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (1, 3)");
+
+            db.execSQL("INSERT INTO cycle (nom, repetition) VALUES(\"cycle abdos + fentes\", 2);");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (2, 2)");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (2, 4)");
+
+            db.execSQL("INSERT INTO cycle (nom, repetition) VALUES(\"cycle Burpees + Montée de Genoux\", 2);");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (3, 5)");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (3, 8)");
+
+            db.execSQL("INSERT INTO cycle (nom, repetition) VALUES(\"cycle Saut + Sprint\", 2);");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (4, 6)");
+            db.execSQL("INSERT INTO CycleTravailCrossRef (cycleId, travailId) VALUES (4, 7)");
+
+            db.execSQL("INSERT INTO sequence (nom, repetition, tempsReposLong, description) VALUES(\"Musculation\", 2, 60,\"Sequence dédiée à la musculation\");");
+            db.execSQL("INSERT INTO SequenceCycleCrossRef (sequenceId, cycleId) VALUES(1,1)");
+            db.execSQL("INSERT INTO SequenceCycleCrossRef (sequenceId, cycleId) VALUES(1,2)");
+
+            db.execSQL("INSERT INTO sequence (nom, repetition, tempsReposLong, description) VALUES(\"Cardio\", 2, 60,\"Sequence dédiée au cardio\");");
+            db.execSQL("INSERT INTO SequenceCycleCrossRef (sequenceId, cycleId) VALUES(2,3)");
+            db.execSQL("INSERT INTO SequenceCycleCrossRef (sequenceId, cycleId) VALUES(2,4)");
+
+            db.execSQL("INSERT INTO entrainement (nom, tempsPreparation, description, tempsRepos) VALUES(\"Entrainement intégré\", 10, \"Contient deux séquences, une muscu et une cardio\", 60)");
+            db.execSQL("INSERT INTO EntrainementSequenceCrossRef (entrainementId, sequenceId) VALUES(1,1)");
+            db.execSQL("INSERT INTO EntrainementSequenceCrossRef (entrainementId, sequenceId) VALUES(1,2)");
+
+
+
+
+
+
         }
     };
 }
